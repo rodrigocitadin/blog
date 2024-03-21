67fe96fd1769a8e4c6be5e3dff5248a02481d2ed
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-21 11:58:55 -0300

    probably done

--- app/page.tsx
+++ app/page.tsx
@@ -9,23 +9,24 @@ import { useRef } from 'react';
 export default function Home() {
   function Torus(props: ThreeElements['mesh']) {
     const ref = useRef<THREE.Mesh>(null!);
-    useFrame((_, delta) => {
-      ref.current.rotation.x += delta;
-      ref.current.rotation.y += delta;
+    useFrame(() => {
+      ref.current.rotation.x += 0.005;
+      ref.current.rotation.y += 0.005;
     });
 
     return (
       <mesh
         {...props}
         ref={ref}
-        scale={0.02}
+        scale={0.03}
       >
         <torusKnotGeometry args={[70, 30, 80, 10]} />
-        <meshPhysicalMaterial 
-          color={0x194c7f} 
-          polygonOffset
-          polygonOffsetFactor={1}
-          polygonOffsetUnits={1}
+        <meshStandardMaterial
+          wireframe
+          color='black'
+          emissive='white'
+          roughness={1}
+          metalness={0.5}
         />
       </mesh>
     )
@@ -40,9 +41,6 @@ export default function Home() {
 
       {/* mobius strip here */}
       <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Torus position={[0, 0, 0]} />
       </Canvas>
 
