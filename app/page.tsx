5a3d5224b8f0b602b372b4a3ae736f1baa620112
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-20 23:18:55 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
+import ListIcon from "./icons/ListIcon";
+
 export default function Home() {
   return (
-    <main>
-      <h1>
-        aaa
-      </h1>
+    <main className="w-[90%] m-auto">
+      <nav className="flex justify-between">
+        <Link href="/">c.dev</Link>
+        <ListIcon/>
+      </nav>
     </main>
   );
 }
