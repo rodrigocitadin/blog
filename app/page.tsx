3f10b54dfb3392f7482b0919da4bd82a1736fffe
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-21 17:11:09 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ListIcon from "./icons/ListIcon";
 import TorusKnot from "./components/TorusKnot";
-import Image from 'next/image';
+import Resume from "./components/Resume";
 
 export default function Home() {
   return (
@@ -17,18 +17,7 @@ export default function Home() {
       <TorusKnot />
 
       {/* my pic and texts here */}
-      <div className="mt-32 w-[90%] m-auto">
-        <Image
-          className="rounded-md m-auto opacity-90 shadow-md shadow-black-600"
-          src="/avatar.jpg"
-          alt="random guy"
-          width={300}
-          height={366}
-        />
-        <p className="mt-4 text-lg">
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-        </p>
-      </div>
+      <Resume />
     </main>
   );
 }
