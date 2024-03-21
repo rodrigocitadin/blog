c717ffb6f13b742a930c5942073b1a90078c6512
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-21 17:12:38 -0300

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="text-black-900">
       <div className="z-50 fixed w-full top-0">
-        <nav className="mt-2 w-[90%] m-auto flex justify-between">
+        <nav className="mt-2 w-[90%] md:w-[80%] xl:w-[50%] m-auto flex justify-between">
           <Link href="/">c.dev</Link>
           <ListIcon />
         </nav>
