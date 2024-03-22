5199bc2cb4c40bfb859794356ed5ff99fce86e1c
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-22 15:07:05 -0300

    open and close navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -10,20 +10,26 @@ export default function Nav() {
 
   return (
     <header className="z-50 fixed w-full top-0">
-      <nav className="items-center mt-2 w-[90%] md:w-[80%] xl:w-[50%] m-auto flex justify-between">
+      <nav className="items-start mt-2 w-[90%] md:w-[80%] xl:w-[50%] m-auto flex justify-between">
         <Link className="text-2xl" href="/">c.dev</Link>
-        <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}>
-          <ListIcon />
-        </button>
+        <div className="flex flex-col items-end">
+          <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}>
+            <ListIcon />
+          </button>
+          <div className={clsx(
+            'grid grid-rows-[0fr] transition-all duration-500 ',
+            { 'grid-rows-[1fr]': asideOpen }
+          )}>
+            <aside className={clsx(
+              'overflow-hidden flex flex-col items-end mt-2',
+              { '!grid-rows-[1fr]': asideOpen }
+            )}>
+              <span>blog</span>
+              <span>portfolio</span>
+            </aside>
+          </div>
+        </div>
       </nav>
-      <aside className={clsx(
-        'hidden',
-        {
-          '!block': asideOpen
-        }
-      )}>
-        hello
-      </aside>
     </header>
   )
 }
