fee6a3e734df97bb91fe4b332f29726936351499
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-28 15:59:44 -0300

    wip

---
+++ app/components/Projects.tsx
@@ -0,0 +1,23 @@
+export default function Projects() {
+  return (
+    <div className="w-[90%] m-auto mt-12">
+      <h2 className="mb-8 text-2xl font-bold">
+        Projects
+      </h2>
+      <div className="flex flex-col gap-4">
+        <img
+          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=moneyc&theme=graywhite"
+          alt="moneyc pin"
+        />
+        <img
+          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=pomotimer&theme=graywhite"
+          alt="moneyc pin"
+        />
+        <img
+          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=grokking-algorithms&theme=graywhite"
+          alt="moneyc pin"
+        />
+      </div>
+    </div>
+  )
+}
