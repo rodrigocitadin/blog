ab98af565fee4fdba4d3a3b769e77a4219069677
rodrigocitadin <citadinrodrigo@gmail.com>
2024-03-28 16:02:21 -0300

    wip

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,13 +1,10 @@
 'use client'
 
-import ListIcon from "@/app/icons/ListIcon";
 import clsx from "clsx";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import CloseIcon from "../icons/CloseIcon";
 
 export default function Nav() {
-  const [asideOpen, setAsideOpen] = useState(false);
   const [bgColor, setBgColor] = useState(false);
 
   const listenScrollEvent = () => {
@@ -32,41 +29,41 @@ export default function Nav() {
         >
           c.dev
         </Link>
-        <div className="flex flex-col items-end md:hidden">
-          <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}>
-            {
-              asideOpen
-                ? <CloseIcon />
-                : <ListIcon />
-            }
-          </button>
-          <div className={clsx(
-            'grid grid-rows-[0fr] transition-all duration-300 ',
-            { 'grid-rows-[1fr]': asideOpen }
-          )}>
-            <aside className={clsx(
-              'overflow-hidden flex flex-col items-end mt-2',
-              { '!grid-rows-[1fr]': asideOpen }
-            )}>
-              <Link className="text-xl" href="/blog">blog</Link>
-              <Link className="text-xl" href="/projects">projects</Link>
-            </aside>
-          </div>
-        </div>
-        <div className="hidden md:flex gap-8">
-          <Link
-            className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900"
-            href="/blog"
-          >
-            blog
-          </Link>
-          <Link
-            className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900"
-            href="/projects"
-          >
-            projects
-          </Link>
-        </div>
+        {/* <div className="flex flex-col items-end md:hidden"> */}
+        {/*   <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}> */}
+        {/*     { */}
+        {/*       asideOpen */}
+        {/*         ? <CloseIcon /> */}
+        {/*         : <ListIcon /> */}
+        {/*     } */}
+        {/*   </button> */}
+        {/*   <div className={clsx( */}
+        {/*     'grid grid-rows-[0fr] transition-all duration-300 ', */}
+        {/*     { 'grid-rows-[1fr]': asideOpen } */}
+        {/*   )}> */}
+        {/*     <aside className={clsx( */}
+        {/*       'overflow-hidden flex flex-col items-end mt-2', */}
+        {/*       { '!grid-rows-[1fr]': asideOpen } */}
+        {/*     )}> */}
+        {/*       <Link className="text-xl" href="/blog">blog</Link> */}
+        {/*       <Link className="text-xl" href="/projects">projects</Link> */}
+        {/*     </aside> */}
+        {/*   </div> */}
+        {/* </div> */}
+        {/* <div className="hidden md:flex gap-8"> */}
+        {/*   <Link */}
+        {/*     className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900" */}
+        {/*     href="/blog" */}
+        {/*   > */}
+        {/*     blog */}
+        {/*   </Link> */}
+        {/*   <Link */}
+        {/*     className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900" */}
+        {/*     href="/projects" */}
+        {/*   > */}
+        {/*     projects */}
+        {/*   </Link> */}
+        {/* </div> */}
       </nav>
     </header>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import TorusKnot from "./components/TorusKnot";
 import Resume from "./components/Resume";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <main className="text-black-900">
       <TorusKnot />
       <Resume />
+      <Projects />
     </main>
   );
 }
