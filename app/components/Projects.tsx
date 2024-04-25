00672bedfb16ea62dfc6d98197f2194e07a1edc1
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-24 21:30:55 -0300

    project card component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,21 +1,31 @@
+function ProjectCard({ imageSrc, desc }: { imageSrc: string, desc: string }) {
+  return (
+    <img
+      className="max-w-[450px]"
+      src={imageSrc}
+      alt={desc}
+    />
+  )
+}
+
 export default function Projects() {
   return (
-    <div className="w-[90%] m-auto mt-12">
+    <div className="mt-12">
       <h2 className="mb-8 text-2xl font-bold">
         Projects
       </h2>
       <div className="flex flex-col gap-4">
-        <img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=moneyc&theme=graywhite"
-          alt="moneyc pin"
+        <ProjectCard
+          imageSrc="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=moneyc&theme=graywhite"
+          desc="moneyc pin"
         />
-        <img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=pomotimer&theme=graywhite"
-          alt="moneyc pin"
+        <ProjectCard
+          imageSrc="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=pomotimer&theme=graywhite"
+          desc="moneyc pin"
         />
-        <img
-          src="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=grokking-algorithms&theme=graywhite"
-          alt="moneyc pin"
+        <ProjectCard
+          imageSrc="https://github-readme-stats.vercel.app/api/pin/?username=rodrigocitadin&repo=grokking-algorithms&theme=graywhite"
+          desc="moneyc pin"
         />
       </div>
     </div>
