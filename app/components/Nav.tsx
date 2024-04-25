7e5e73b1e83c2ef664e818c65adfa0403fe0ead0
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-24 21:40:54 -0300

    remove aside comments

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -3,9 +3,12 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import CloseIcon from "@/app/icons/CloseIcon";
+import ListIcon from "@/app/icons/ListIcon";
 
 export default function Nav() {
   const [bgColor, setBgColor] = useState(false);
+  const [asideOpen, setAsideOpen] = useState(false);
 
   const listenScrollEvent = () => {
     window.scrollY < 600
@@ -19,7 +22,7 @@ export default function Nav() {
 
   return (
     <header className={clsx(
-      'z-50 fixed w-full border-b-[1px] border-b-transparent top-0 py-2 transition-all duration-300', 
+      'z-50 fixed w-full border-b-[1px] border-b-transparent top-0 py-2 transition-all duration-300',
       { 'bg-white !border-b-light': bgColor }
     )}>
       <nav className="md:items-center items-start w-[90%] md:w-[80%] xl:w-[50%] m-auto flex justify-between">
@@ -29,41 +32,41 @@ export default function Nav() {
         >
           c.dev
         </Link>
-        {/* <div className="flex flex-col items-end md:hidden"> */}
-        {/*   <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}> */}
-        {/*     { */}
-        {/*       asideOpen */}
-        {/*         ? <CloseIcon /> */}
-        {/*         : <ListIcon /> */}
-        {/*     } */}
-        {/*   </button> */}
-        {/*   <div className={clsx( */}
-        {/*     'grid grid-rows-[0fr] transition-all duration-300 ', */}
-        {/*     { 'grid-rows-[1fr]': asideOpen } */}
-        {/*   )}> */}
-        {/*     <aside className={clsx( */}
-        {/*       'overflow-hidden flex flex-col items-end mt-2', */}
-        {/*       { '!grid-rows-[1fr]': asideOpen } */}
-        {/*     )}> */}
-        {/*       <Link className="text-xl" href="/blog">blog</Link> */}
-        {/*       <Link className="text-xl" href="/projects">projects</Link> */}
-        {/*     </aside> */}
-        {/*   </div> */}
-        {/* </div> */}
-        {/* <div className="hidden md:flex gap-8"> */}
-        {/*   <Link */}
-        {/*     className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900" */}
-        {/*     href="/blog" */}
-        {/*   > */}
-        {/*     blog */}
-        {/*   </Link> */}
-        {/*   <Link */}
-        {/*     className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900" */}
-        {/*     href="/projects" */}
-        {/*   > */}
-        {/*     projects */}
-        {/*   </Link> */}
-        {/* </div> */}
+        <div className="flex flex-col items-end md:hidden">
+          <button onClick={() => { setAsideOpen(!asideOpen); console.log(asideOpen) }}>
+            {
+              asideOpen
+                ? <CloseIcon />
+                : <ListIcon />
+            }
+          </button>
+          <div className={clsx(
+            'grid grid-rows-[0fr] transition-all duration-300 ',
+            { 'grid-rows-[1fr]': asideOpen }
+          )}>
+            <aside className={clsx(
+              'overflow-hidden flex flex-col items-end mt-2',
+              { '!grid-rows-[1fr]': asideOpen }
+            )}>
+              <Link className="text-xl" href="/blog">blog</Link>
+              <Link className="text-xl" href="/projects">projects</Link>
+            </aside>
+          </div>
+        </div>
+        <div className="hidden md:flex gap-8">
+          <Link
+            className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900"
+            href="/blog"
+          >
+            blog
+          </Link>
+          <Link
+            className="transition-all duration-300 text-2xl border-b-2 border-t-2 border-transparent hover:border-b-black-900"
+            href="/projects"
+          >
+            projects
+          </Link>
+        </div>
       </nav>
     </header>
   )
