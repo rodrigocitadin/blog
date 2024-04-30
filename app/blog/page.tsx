20f2b6a7d044f407895027c0eb295a74c62452dd
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-29 23:14:16 -0300

    testing new markdown parser

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,17 @@
+import markdownIt from "markdown-it"
+
 export default function Blog() {
+  const md = markdownIt()
+  const result = md.parse('# Sla', {})
+
   return (
-    <h1 className="text-2xl underline mt-40">TODO blog</h1>
+    <div>
+      <h1 className="text-2xl underline mt-40">TODO blog</h1>
+      <pre>
+        {result.map((v) => {
+          return <h1>{v.content}</h1>
+        })}
+      </pre>
+    </div>
   )
 }
