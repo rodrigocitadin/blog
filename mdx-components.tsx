4490104f1c58176115e118cf864c8b505cba45b7
rodrigocitadin <citadinrodrigo@gmail.com>
2024-05-04 16:27:30 -0300

    some styles to mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -3,9 +3,24 @@ import type { MDXComponents } from 'mdx/types'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => (
-      <h1 className="text-blue-500">
+      <h1 className="mt-32 font-bold text-4xl text-black-900">
         {children}
       </h1>
+    ),
+    h2: ({ children }) => (
+      <h2 className="font-bold text-2xl text-black-900">
+        {children}
+      </h2>
+    ),
+    h3: ({ children }) => (
+      <h3 className="font-bold text-xl text-black-900">
+        {children}
+      </h3>
+    ),
+    li: ({ children }) => (
+      <li className="pl-8 before:content-['-_'] text-black-900">
+        {children}
+      </li>
     ),
     ...components,
   }
