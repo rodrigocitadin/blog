d4872a86d9802f0f130cb3f0b3350dac48403acf
rodrigocitadin <citadinrodrigo@gmail.com>
2024-05-04 16:13:46 -0300

    config tailwind into mdx

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,12 @@
 import type { MDXComponents } from 'mdx/types'
- 
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => (
+      <h1 className="text-blue-500">
+        {children}
+      </h1>
+    ),
     ...components,
   }
 }
