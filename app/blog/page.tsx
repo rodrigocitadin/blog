cae72d98a002bad7f6ed37cf417dcf0ad6f0e057
rodrigocitadin <citadinrodrigo@gmail.com>
2024-05-04 14:04:30 -0300

    wip wip wip

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,7 +4,7 @@ import Sla from './markdowns/sla.md'
 
 export default function Blog() {
   return (
-    <div>
+    <div className="w-[90%] md:w-[80%] xl:w-[50%] m-auto">
       <h1 className="text-2xl underline mt-40">TODO blog</h1>
       <Sla />
     </div>
--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
 })
 
-module.exports = withMDX()
+module.exports = withMDX(nextConfig)
