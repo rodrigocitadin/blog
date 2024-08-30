90d9caaa8a247c45e4ce4f45f2750bb41eccdb12
rodrigocitadin <citadinrodrigo@gmail.com>
2024-08-30 10:39:20 -0300

    feat: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col mt-10">
+      <h1 className="text-7xl leading-none">404</h1>
+      <h2 className="text-3xl leading-none">Page not found</h2>
+    </div>
+  )
+}
